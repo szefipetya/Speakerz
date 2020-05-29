8608b39db03549eb5018f06e9179dd762b5f37e1
Kovács Tamás <gjamtr@inf.elte.hu>
2020-05-29 17:13:35 +0200

    2 Activity join button moves you to the second one

--- app/src/main/java/com/example/speakerz/MainActivity.java
+++ app/src/main/java/com/example/speakerz/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.speakerz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button Activity2 = (Button) findViewById(R.id.join);
+        Activity2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent Act2 = new Intent(getApplicationContext(),Join.class);
+                Act2.putExtra("Hello","Hello World");
+                startActivity(Act2);
+
+            }
+
+        });
+
+
+
+
+
+
+
     }
+
+
+
+
+
 }
