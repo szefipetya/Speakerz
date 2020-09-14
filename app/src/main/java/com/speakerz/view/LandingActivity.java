b639c7ff5df266f32ed8d5da5b4293138215686c
Juhász Dávid <u9uhuy@inf.elte.hu>
2020-09-14 15:39:33 +0200

    Landing Screen with animation

---
+++ app/src/main/java/com/speakerz/view/LandingActivity.java
@@ -0,0 +1,24 @@
+package com.speakerz.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import com.speakerz.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_layout);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
